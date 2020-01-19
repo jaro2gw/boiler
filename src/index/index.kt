2bf60ce6893bb22d29b944c4c373359d8addaed1
Jaro <p.jaronsky@gmail.com>
2020-01-19 16:43:13 +0100

    Refactor code

--- src/index/index.kt
+++ src/index/index.kt
@@ -3,27 +3,16 @@ package index
 import boiler.boiler
 import kotlinext.js.require
 import kotlinext.js.requireAll
-import kotlinx.css.*
+import react.dom.div
 import react.dom.h2
 import react.dom.render
-import styled.css
-import styled.styledDiv
 import kotlin.browser.document
 
 fun main() {
     requireAll(require.context("src", true, js("/\\.css$/")))
 
     render(document.getElementById("root")) {
-        styledDiv {
-            css {
-                height = 60.px
-                padding(20.px)
-                backgroundColor = Color("#0096ff")
-                color = Color.white
-            }
-            h2 { +"Boiler Simulator" }
-        }
-
+        div("header") { h2 { +"Boiler Simulator" } }
         boiler { specificHeatCapacity = 4200.0 }
     }
 }
